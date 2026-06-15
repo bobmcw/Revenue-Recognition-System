3bbed995cbf6239c666ebd78da5612975b302d02
bobmcw <jasiekz2014@gmail.com>
2026-06-15 17:54:04 +0200

    added configuration in program.cs

--- Revenue-Recognition-System/Program.cs
+++ Revenue-Recognition-System/Program.cs
@@ -1,10 +1,15 @@
+using Microsoft.EntityFrameworkCore;
+using Revenue_Recognition_System.Infrastructure;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-
 builder.Services.AddControllers();
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
+
+builder.Services.AddDbContext<DatabaseContext>(opt =>
+{
+    opt.UseSqlServer(builder.Configuration.GetConnectionString("Default"));
+});
 
 var app = builder.Build();
 
