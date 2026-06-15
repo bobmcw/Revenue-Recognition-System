57277756520f2bcb9986eb55fe673d2b5e2f4520
bobmcw <jasiekz2014@gmail.com>
2026-06-15 17:42:51 +0200

    removed unneeded import

--- Revenue-Recognition-System/Models/IndividualClient.cs
+++ Revenue-Recognition-System/Models/IndividualClient.cs
@@ -1,6 +1,5 @@
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
-using Revenue_Recognition_System.BaseClasses;
 
 namespace Revenue_Recognition_System.Models;
 
