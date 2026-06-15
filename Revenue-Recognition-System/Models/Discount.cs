bacb1079d3a0592610e24a0659af7ed1c47cce4c
bobmcw <jasiekz2014@gmail.com>
2026-06-15 17:42:27 +0200

    discount model

---
+++ Revenue-Recognition-System/Models/Discount.cs
@@ -0,0 +1,18 @@
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Revenue_Recognition_System.Models;
+
+[Table("Discounts")]
+public class Discount
+{
+   [Key]
+   public int Id { get; set; }
+   [MaxLength(100)]
+   public string Name { get; set; } = string.Empty;
+   public decimal DiscountPercentage { get; set; }
+   public DateTime StartDate { get; set; } 
+   public DateTime EndDate { get; set; }
+
+   public IEnumerable<Product> Products { get; set; } = [];
+}
