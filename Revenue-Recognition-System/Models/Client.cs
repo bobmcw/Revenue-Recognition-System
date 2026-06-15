46c909392e910224dc543335427fbd3c9a83fca7
bobmcw <jasiekz2014@gmail.com>
2026-06-15 17:36:57 +0200

    made Client into a model

--- Revenue-Recognition-System/Models/Client.cs
+++ Revenue-Recognition-System/Models/Client.cs
@@ -1,17 +1,21 @@
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using Revenue_Recognition_System.Enums;
 
-namespace Revenue_Recognition_System.BaseClasses;
+namespace Revenue_Recognition_System.Models;
 
+[Table("Clients")]
 public abstract class Client
 {
    [Key]
    public int Id { get; set; }
-   public ClientType ClientType { get; set; }
+   // public ClientType ClientType { get; set; }
    
    //contact info
    [MaxLength(50)]
    public string Email { get; set; } = string.Empty;
    [MaxLength(15)]
    public string PhoneNumber { get; set; } = string.Empty;
+
+   public IEnumerable<Contract> Contracts { get; set; } = [];
 }
