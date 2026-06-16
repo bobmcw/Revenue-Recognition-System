87b3a3d633b7e3afda735e23b04a90a8b9ac2491
bobmcw <jasiekz2014@gmail.com>
2026-06-16 14:17:30 +0200

    refreshtoken update migration

--- Revenue-Recognition-System/Models/RefreshToken.cs
+++ Revenue-Recognition-System/Models/RefreshToken.cs
@@ -1,10 +1,13 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace Revenue_Recognition_System.Models;
 
 public class RefreshToken
 {
+    [Key]
     public int Id { get; set; }
+    [MaxLength(8000)]
     public string Token { get; set; } = string.Empty;
-    public string JwtId { get; set; } = string.Empty;
     public DateTime CreationDate { get; set; }
     public DateTime ExpiryDate { get; set; }
 
