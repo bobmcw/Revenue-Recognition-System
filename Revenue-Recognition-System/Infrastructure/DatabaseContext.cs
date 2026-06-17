4e51dee94e01a1fea015dc6fbf80733aadaaf34b
bobmcw <jasiekz2014@gmail.com>
2026-06-17 15:41:29 +0200

    added Payments table

--- Revenue-Recognition-System/Infrastructure/DatabaseContext.cs
+++ Revenue-Recognition-System/Infrastructure/DatabaseContext.cs
@@ -13,6 +13,7 @@ public class DatabaseContext(DbContextOptions opt) : DbContext(opt)
    public DbSet<Product> Products { get; set; }
    public DbSet<User> Users { get; set; }
    public DbSet<RefreshToken> RefreshTokens { get; set; }
+   public DbSet<Payment> Payments { get; set; }
 
    protected override void OnModelCreating(ModelBuilder modelBuilder)
    {
