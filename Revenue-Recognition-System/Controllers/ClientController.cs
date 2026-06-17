bfb718e2ec565ae869494ad047bf929ba9b07972
bobmcw <jasiekz2014@gmail.com>
2026-06-17 12:08:42 +0200

    delete and put endpoints for admin users

--- Revenue-Recognition-System/Controllers/ClientController.cs
+++ Revenue-Recognition-System/Controllers/ClientController.cs
@@ -1,5 +1,6 @@
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Revenue_Recognition_System.Enums;
 using Revenue_Recognition_System.Exceptions;
 using Revenue_Recognition_System.PostDTOs;
 using Revenue_Recognition_System.Services;
@@ -26,6 +27,36 @@ public class ClientController(IClientService serv) : ControllerBase
             return Created();
         }
         catch (UnknownClientTypeException e)
+        {
+            return BadRequest(e.Message);
+        }
+    }
+
+    [HttpPut]
+    [Authorize(Roles = nameof(UserRole.Admin))]
+    public async Task<IActionResult> UpdateClientAsync([FromQuery] int id, [FromBody] CreateBaseClientDto dto)
+    {
+        try
+        {
+            await serv.UpdateClient(id, dto);
+            return NoContent();
+        }
+        catch (IllegalModificationException e)
+        {
+            return BadRequest(e.Message);
+        }
+    }
+
+    [HttpDelete]
+    [Authorize(Roles = nameof(UserRole.Admin))]
+    public async Task<IActionResult> DeleteClientAsync([FromQuery] int id)
+    {
+        try
+        {
+            await serv.DeleteClient(id);
+            return NoContent();
+        }
+        catch (IllegalModificationException e)
         {
             return BadRequest(e.Message);
         }
